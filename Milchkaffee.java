d1bf965959b3252c65e16c6c1229ea7622952adc
Hendrik Sollich <hoodie@users.noreply.github.com>
2016-07-27 17:17:17 +0200

    Update Milchkaffee.java
    Java hat ägyptische  Klammern oder?

--- Milchkaffee.java
+++ Milchkaffee.java
@@ -1,9 +1,7 @@
 // "Das haben wir schon immer getrunken
 // und das werden wir auch weiterhin trinken!"
-class Milchkaffee
-{
- public static void main(String[] args)
- {
-  System.out.println("Milchkaffee - 1,00€");
- }
+class Milchkaffee{
+  public static void main(String[] args){
+    System.out.println("Milchkaffee - 1,00€");
+  }
 }
